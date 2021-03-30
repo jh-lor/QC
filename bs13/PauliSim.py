98cc987b6d91f782c2cdd2530c383fc03d2e5f58
jhlor <jl850@duke.edu>
2021-03-29 20:13:57 -0400

    wrote depolarizing noise

--- bs13/PauliSim.py
+++ bs13/PauliSim.py
@@ -10,6 +10,12 @@ class PauliSim():
             operation(self.state)
         return self.state.astype(int)
 
+    def add(self, operation):
+        self.operations.append(operation)
+
+    def addDepolarizingNoise(self, qubits, p):
+        self.operations.append(DepolarizingNoise(self,qubits,p).addNoise)
+
     def addX(self, target):
         self.operations.append(Gates(target).X)
     
@@ -49,6 +55,10 @@ class Gates():
         self.gate = None
         self.target = target
         self.control = control 
+        self.str = ""
+    
+    def I(self, state):
+        pass
 
     def X(self, state):
         state[self.target][0] = not state[self.target][0]
@@ -80,40 +90,76 @@ class Gates():
         self.H(state)
         self.str = "CZ(",self.control,",",self.target,")"
 
-    # def __str__(self):
-    #     return self.str
+    def __str__(self):
+        return self.str
+
+class DepolarizingNoise():
+    def __init__(self, paulisim, qubits, p):
+        self.qubits = qubits
+        self.number = len(qubits)
+        self.rate = p
+        self.operations = paulisim.operations
+
+    def addNoise(self, state):
+        # only implemented noise for 1 and 2 qubits
+        seed = np.random.rand(self.number)
+        
+        for i in range(len(self.qubits)):
+            random = seed[i]
+            qubit = self.qubits[i]
+            if random < self.rate:
+                if self.number == 1:
+                    if random < self.rate/3:
+                        self.operations.append(Gates(qubit).X)
+                    elif random < 2*self.rate/3:
+                        self.operations.append(Gates(qubit).Y)
+                    else:
+                        self.operations.append(Gates(qubit).Z)
+                if self.number == 2:
+                    if random < 4*self.rate/15:
+                        self.operations.append(Gates(qubit).X)
+                    elif random < 8*self.rate/15:
+                        self.operations.append(Gates(qubit).Y)
+                    elif random < 12*self.rate/15:
+                        self.operations.append(Gates(qubit).Z)
+
+
+        
 
 
 if __name__ == "__main__":
-    sim = PauliSim(13)
+    # sim = PauliSim(13)
 
-    sim.addCNOT(0,3) 
-    sim.addCNOT(0,6)  
+    # sim.addCNOT(0,3) 
+    # sim.addCNOT(0,6)  
 
-    for i in [0,3,6]:
-        sim.addH(i)
+    # for i in [0,3,6]:
+    #     sim.addH(i)
 
-    sim.addCNOT(0,1)
-    sim.addCNOT(0,2)
+    # sim.addCNOT(0,1)
+    # sim.addCNOT(0,2)
 
-    sim.addCNOT(3,4)
-    sim.addCNOT(3,5)
+    # sim.addCNOT(3,4)
+    # sim.addCNOT(3,5)
 
-    sim.addCNOT(6,7)
-    sim.addCNOT(6,8)
+    # sim.addCNOT(6,7)
+    # sim.addCNOT(6,8)
 
-    for i in range(0,9):
-        sim.addH(i)
+    # for i in range(0,9):
+    #     sim.addH(i)
 
-    # Add Errors Here
-    sim.addZ(1)
+    # # Add Errors Here
+    # sim.addZ(1)
 
-    sim.addZStabilizer([0,3,1,4,2,5], 9)
-    sim.addZStabilizer([3,6,4,7,5,8], 10)
-    sim.addXStabilizer([0,1,3,4,6,7], 11)
-    sim.addXStabilizer([1,2,4,5,7,8], 12)
-
-    print(sim.execute())
+    # sim.addZStabilizer([0,3,1,4,2,5], 9)
+    # sim.addZStabilizer([3,6,4,7,5,8], 10)
+    # sim.addXStabilizer([0,1,3,4,6,7], 11)
+    # sim.addXStabilizer([1,2,4,5,7,8], 12)
+    for i in range(100):
+        sim = PauliSim(2)
+        sim.addDepolarizingNoise([0,1],0.5)
+        print(sim.execute())
+        print("\n")
     
     
 
